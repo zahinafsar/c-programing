5ec43432d33bf7e5e1ccf502e073129d7de98434
Zahin Afsar <43959210+Mdzahin@users.noreply.github.com>
2019-04-30 12:25:47 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+void filecreat(){
+
+FILE* output=fopen("sd.txt","w");
+if (output!=NULL){
+
+char name[10],fn[10],mn[10],add[10];
+int ag;
+printf("What is your name?");
+scanf("%s",name);
+printf("What is your fathers name?");
+scanf("%s",fn);
+printf("What is your mother name?");
+scanf("%s",mn);
+printf("Where do you live in?");
+scanf("%s",add);
+printf("What is your age?");
+scanf("%d",&ag);
+fprintf(output,"Name:%s\nFathers name:%s\nMothers name:%s\nAddress:%s\nAge:%d",name,fn,mn,add,ag);
+fclose(output);
+printf("You CV has been created succesfully");
+}else{
+printf("Try again");
+}
+
+}
+int main()
+{
+filecreat();
+}
